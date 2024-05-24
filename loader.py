6206606b712e5d466441406f8bb466256efd74c0
Alex Shapiro <shapiro.alex@gmail.com>
2024-05-24 06:47:11 -0400

    use logging

--- loader.py
+++ loader.py
@@ -2,6 +2,11 @@
 import requests
 import threading
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+logger = logging.getLogger(__name__)
 
 TIME_OUT_STATUS = 600
 
@@ -66,11 +71,12 @@ class Loader:
 
     def start(self):
         self.start_time = time.time()
+        logger.info(f"Started loader")
         pass_count = 0
         expected_end_time = self.start_time+self.duration
+        expected_pass_count = self.qps*self.duration
         # we're making requests at an even rate but for tidy accounting make sure we reach
         # expected_pass_count with the very last batch which might take an extra millisecond.
-        expected_pass_count = self.qps*self.duration
         while time.time() < expected_end_time or pass_count < expected_pass_count:
             if pass_count > self.get_rate_limited_max_request_count():
                 # sleep for 1ms to save CPU cycles
@@ -82,11 +88,17 @@ class Loader:
             self.request_passes.append(request_pass)
             request_pass.start()
             pass_count += 1
-
-        print(time.time(), pass_count)
+            if pass_count % 1000 == 0:
+                logger.info(
+                    f"Started {pass_count} requests at {time.time()-self.start_time:.2f}s"
+                )
 
         for lr in self.request_passes:
             lr.thread.join()
+
+        logger.info(
+            f"Completed {pass_count} requests at {time.time()-self.start_time:.2f}s"
+        )
 
         # for the Loader we consider the end_time to be the start_time of
         # the last request (not how long it takes requests to complete)
